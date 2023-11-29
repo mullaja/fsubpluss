f778cec47444bd97408d15f6c9f461c4277f58a5
devolart <125961492+devolart@users.noreply.github.com>
2023-11-29 16:58:16 +0700

    Adding more anti floodwait
    (cherry picked from commit 453385b4e0359012a5ae02708c9c691bbdddc7a3)

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -69,6 +69,9 @@ async def channel_post(client: Client, message: Message):
     if not DISABLE_BUTTON:
         try:
             await post_message.edit_reply_markup(reply_markup)
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await post_message.edit_reply_markup(reply_markup)
         except Exception:
             pass
 
@@ -93,6 +96,9 @@ async def new_post(client: Client, message: Message):
         ]
     )
     try:
+        await message.edit_reply_markup(reply_markup)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
         await message.edit_reply_markup(reply_markup)
     except Exception:
         pass
